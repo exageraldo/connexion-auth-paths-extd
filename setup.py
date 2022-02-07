01f1abf0b4c65b4647c905fa1991563c024fe94c
Geraldo Castro <victormatheuscastro@gmail.com>
2022-02-06 21:52:57 -0300

    Add pypi release workflow and update the version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="connexion_auth_paths_extd",
-    version="0.0.1-dev",
+    version="0.0.1-dev1",
     install_requires=[
         "connexion~=2.10.0"
     ]
