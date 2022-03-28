806c78427a9294d2bbb4b22945a3d9f712e7fdbc
Geraldo Castro <victormatheuscastro@gmail.com>
2022-03-28 17:11:50 -0300

    Update connexion version

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="connexion_auth_paths_extd",
-    version="0.0.2-dev",
+    version="0.0.2-de1v",
     install_requires=[
-        "connexion~=2.10.0"
+        "connexion~=2.10"
     ]
 )
