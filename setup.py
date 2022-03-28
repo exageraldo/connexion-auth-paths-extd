660124a846ab7a43e7e64d46943afabe2871ef69
Geraldo Castro <victormatheuscastro@gmail.com>
2022-03-28 18:05:48 -0300

    Update version to release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="connexion_auth_paths_extd",
-    version="0.0.3",
+    version="0.0.4",
     install_requires=[
         "connexion"
     ],
